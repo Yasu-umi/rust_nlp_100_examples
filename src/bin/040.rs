903a8a23a86de325935990de99af624f44c55b4b
Yasu_umiNishikawa <yasu.umi.19910101@gmail.com>
2017-04-25 11:49:39 +0900

    morphs from sentences

--- src/bin/040.rs
+++ src/bin/040.rs
@@ -9,22 +9,7 @@ use nlp_100_examples::*;
 fn main() {
     let text = fetch::text("http://www.cl.ecei.tohoku.ac.jp/nlp100/data/neko.txt");
 
-    let mut parser = cabocha::Parser::new("");
-    let tree = parser.parse_to_tree(text);
+    let morphed_sentences = structs::Morph::from_sentences(text);
 
-    let mut morphed_sentences = Vec::new();
-    let mut morphed_sentence = Vec::new();
-
-    let mut i = 0;
-    while morphed_sentences.len() < 3 {
-        let morph = structs::Morph::from_cabocha_token(tree.token(i).unwrap());
-        let is_end_of_sentence = morph.is_end_of_sentence();
-        morphed_sentence.push(morph);
-        if is_end_of_sentence {
-            morphed_sentences.push(morphed_sentence);
-            morphed_sentence = Vec::new();
-        }
-        i += 1;
-    }
-    println!("{:?}", morphed_sentences.last().unwrap());
+    println!("{:?}", morphed_sentences.get(2).unwrap());
 }
--- src/structs.rs
+++ src/structs.rs
@@ -2,8 +2,6 @@ extern crate mecab;
 extern crate cabocha;
 
 use std;
-use self::mecab::Node;
-use self::cabocha::Token;
 
 #[derive(Debug)]
 pub struct Morph {
@@ -25,7 +23,7 @@ impl std::fmt::Display for Morph {
 }
 
 impl Morph {
-    pub fn from_mecab_node(node: Node) -> Morph {
+    pub fn from_mecab_node(node: mecab::Node) -> Morph {
         let mut feature = node.feature.split(",");
         Morph {
             surface: (&(node.surface)[..(node.length as usize)]).to_string(),
@@ -35,7 +33,7 @@ impl Morph {
         }
     }
 
-    pub fn from_cabocha_token(token: Token) -> Morph {
+    pub fn from_cabocha_token(token: cabocha::Token) -> Morph {
         let mut feature_iter = token.feature_list.iter();
         Morph {
             surface: token.surface,
@@ -43,6 +41,26 @@ impl Morph {
             pos1: feature_iter.nth(0).map(|t| t.to_string()).unwrap_or("".to_string()),
             base: feature_iter.nth(4).map(|t| t.to_string()).unwrap_or("".to_string()),
         }
+    }
+
+    pub fn from_sentences(text: String) -> Vec<Vec<Morph>> {
+        let mut parser = cabocha::Parser::new("");
+        let tree = parser.parse_to_tree(text);
+        let token_size = tree.token_size();
+
+        let mut morphed_sentences = Vec::new();
+        let mut morphed_sentence = Vec::new();
+
+        for i in 0..token_size {
+            let morph = Morph::from_cabocha_token(tree.token(i).unwrap());
+            let is_end_of_sentence = morph.is_end_of_sentence();
+            morphed_sentence.push(morph);
+            if is_end_of_sentence {
+                morphed_sentences.push(morphed_sentence);
+                morphed_sentence = Vec::new();
+            }
+        }
+        morphed_sentences
     }
 
     pub fn is_end_of_sentence(&self) -> bool {
