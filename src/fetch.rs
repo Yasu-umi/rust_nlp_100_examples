42c04ea1c9665cc6b94ed0955d8a3cf06ddec0d1
Yasu_umiNishikawa <yasu.umi.19910101@gmail.com>
2017-06-08 20:57:46 +0900

    tar_gz_files

--- src/fetch.rs
+++ src/fetch.rs
@@ -2,8 +2,10 @@ extern crate hyper;
 extern crate flate2;
 extern crate serde_json;
 extern crate regex;
+extern crate tar;
 
 use std::io;
+use std::io::{Read, BufRead, BufReader, ErrorKind};
 use std::ops::Index;
 use std::error::Error;
 use std::collections::HashMap;
@@ -12,6 +14,7 @@ use self::hyper::{client, header};
 use self::flate2::read::GzDecoder;
 use self::serde_json::Value;
 use self::regex::Regex;
+use self::tar::Archive;
 
 use artist::Artist;
 
@@ -19,13 +22,14 @@ pub fn create_client() -> client::Client {
     client::Client::new()
 }
 
-pub fn reader(client: client::Client, url: &str) -> Result<impl io::BufRead, Box<Error>> {
+pub fn reader(client: client::Client, url: &str)
+    -> Result<impl BufRead, Box<Error>> {
     let res = try!(client.get(url).send());
-    Ok(io::BufReader::new(res))
+    Ok(BufReader::new(res))
 }
 
-pub fn string(client: client::Client, url: &str) -> Result<String, Box<Error>> {
-    use std::io::Read;
+pub fn string(client: client::Client, url: &str)
+    -> Result<String, Box<Error>> {
     let mut reader = try!(reader(client, url));
     let mut text = String::new();
     reader.read_to_string(&mut text)?;
@@ -33,7 +37,7 @@ pub fn string(client: client::Client, url: &str) -> Result<String, Box<Error>> {
 }
 
 pub fn gz_reader(client: client::Client, url: &str)
-    -> Result<impl io::BufRead, Box<Error>> {
+    -> Result<impl BufRead, Box<Error>> {
     let mut req = client.get(url);
     let mut headers = header::Headers::new();
     headers.set(header::AcceptEncoding(vec![header::qitem(header::Encoding::Gzip)]));
@@ -41,12 +45,32 @@ pub fn gz_reader(client: client::Client, url: &str)
 
     let res = try!(req.send());
     let decorder = try!(GzDecoder::new(res));
-    Ok(io::BufReader::new(decorder))
+    Ok(BufReader::new(decorder))
+}
+
+pub fn tar_gz_files<'a>(client: client::Client, url: String)
+    -> Result<Vec<(Vec<u8>, String)>, Box<Error>> {
+    let gz_reader = try!(gz_reader(client, url.as_str()));
+    let mut archive = Archive::new(gz_reader);
+    let entries = try!(archive.entries());
+    Ok(entries.filter_map(|entry| {
+        entry.and_then(|mut file| {
+            let mut buffer = Vec::new();
+            let _ = file.read_to_end(&mut buffer)?;
+            let path = try!(
+                try!(file.header().path())
+                    .to_str()
+                    .map(|path_str| path_str.to_string())
+                    .ok_or(io::Error::new(ErrorKind::Other, "Failed to parse path"))
+                );
+            Ok((buffer, path))
+        }).ok()
+    }).collect())
 }
 
 pub fn gz_string_by_line<'a>(url: &'a str)
     -> Result<impl Iterator<Item=String> + 'a, Box<Error>> {
-    use self::io::BufRead;
+    use self::BufRead;
     let reader = try!(gz_reader(create_client(), url));
     Ok(reader.lines().filter_map(|res_line)
         if let Ok(line) = res_line { Some(line) } else { None }
@@ -83,8 +107,9 @@ pub fn country_texts<'a>(url: &'a str, country_re: &str)
     }))
 }
 
-pub fn get_template_hash<F: Fn(String) -> String>(url: &str, country: &str, formatter: F)
-    -> Result<HashMap<String, String>, Box<Error>> {
+pub fn get_template_hash<F>(url: &str, country: &str, formatter: F)
+    -> Result<HashMap<String, String>, Box<Error>>
+    where F: Fn(String) -> String {
     let spliter = try!(Regex::new(r"\n[\|}]"));
     let re = try!(Regex::new(r"(?s)^(.*?)\s=\s(.*)(?-s)$"));
     let mut hash = HashMap::new();
